105e3ac22f36d4fd4261b46867906f9a03c70205
shubhammusic00 <120797454+shubhammusic00@users.noreply.github.com>
2022-12-21 09:25:21 +0530

    Update help.py

--- AnonX/utils/inline/help.py
+++ AnonX/utils/inline/help.py
@@ -26,57 +26,57 @@ def help_pannel(_, START: Union[bool, int] = None):
         [
             [
                 InlineKeyboardButton(
-                    text="🎼ᴀᴅᴍɪɴ🎼",
+                    text="💥ᴀᴅᴍɪɴ",
                     callback_data="help_callback hb1",
                 ),
                 InlineKeyboardButton(
-                    text="🎼ᴀᴜᴛʜ🎼",
+                    text="💥ᴀᴜᴛʜ💥",
                     callback_data="help_callback hb2",
                 ),
                 InlineKeyboardButton(
-                    text="🎼ʙʟᴀᴄᴋʟɪsᴛ🎼",
+                    text="💥ʙʟᴀᴄᴋʟɪsᴛ💥",
                     callback_data="help_callback hb3",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="🎼ʙʀᴏᴀᴅᴄᴀsᴛ🎼",
+                    text="💥ʙʀᴏᴀᴅᴄᴀsᴛ☀️",
                     callback_data="help_callback hb4",
                 ),
                 InlineKeyboardButton(
-                    text="🎼ɢʙᴀɴ🎼",
+                    text="💥ɢʙᴀɴ💥",
                     callback_data="help_callback hb12",
                 ),
                 InlineKeyboardButton(
-                    text="🎼ʟʏʀɪᴄs🎼",
+                    text="💥ʟʏʀɪᴄs💥",
                     callback_data="help_callback hb5",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="🎼ᴩɪɴɢ🎼",
+                    text="💥ᴩɪɴɢ💥",
                     callback_data="help_callback hb7",
                 ),
                 InlineKeyboardButton(
-                    text="🎼ᴩʟᴀʏ🎼",
+                    text="💥ᴩʟᴀʏ💥",
                     callback_data="help_callback hb8",
                 ),
                 InlineKeyboardButton(
-                    text="🎼ᴩʟᴀʏʟɪsᴛ🎼",
+                    text="💥ᴩʟᴀʏʟɪsᴛ💥",
                     callback_data="help_callback hb6",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="🎼ᴠɪᴅᴇᴏᴄʜᴀᴛs🎼",
+                    text="💥ᴠɪᴅᴇᴏᴄʜᴀᴛs💥",
                     callback_data="help_callback hb10",
                 ),
                 InlineKeyboardButton(
-                    text="🎼sᴛᴀʀᴛ🎼",
+                    text="💥sᴛᴀʀᴛ💥",
                     callback_data="help_callback hb11",
                 ),
                 InlineKeyboardButton(
-                    text="🎼sᴜᴅᴏ🎼",
+                    text="💥sᴜᴅᴏ💥",
                     callback_data="help_callback hb9",
                 ),
             ],
@@ -110,7 +110,7 @@ def private_help_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🎼 ʜᴇʟᴩ 🎼",
+                text="💥 ʜᴇʟᴩ 💥",
                 callback_data="settings_back_helper",
             ),
         ],
